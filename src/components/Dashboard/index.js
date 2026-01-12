2a4eaff4e955dcbeeda658459a05caa9fdf1cacf
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2026-01-12 14:47:25 +0100

    firebase credentials, and useeffect fix

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,29 +1,26 @@
 import { useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Home from "./home";
-import Login from '../Login';
+import Login from "../Login";
 
 const Dashboard = () => {
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
 
-    const [user, setUser] = useState(null);
-    const auth = getAuth();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user ? user : null);
+    });
 
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if(user) {
-                setUser(user);
-            } else {
-                setUser(null);
-            }
-        })
-    }, []);
+    // cleanup listener when component unmounts
+    return () => unsubscribe();
+  }, [auth]);
 
-
-    return (
-       <div>
-           {user ? <Home /> : <Login />}
-       </div>
-    )
-}
+  return (
+    <div>
+      {user ? <Home /> : <Login />}
+    </div>
+  );
+};
 
 export default Dashboard;
